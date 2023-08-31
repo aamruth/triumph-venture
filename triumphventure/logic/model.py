fc143f27ef86b305bac97f6a90530affe98f2da4
India Dearlove <india_dearlove@yahoo.co.uk>
2023-08-31 10:40:33 +0200

    Add yaml file

--- triumphventure/logic/model.py
+++ triumphventure/logic/model.py
@@ -1,8 +1,4 @@
-y = encoded_data["Success_failure"]
-X = encoded_data.drop(columns=["Success_failure"])
 from sklearn.model_selection import train_test_split
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
